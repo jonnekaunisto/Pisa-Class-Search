01c90944fb9139cb5a7ca033befdc85d33ffcf1c
Ernesto <ernestosmail45@gmail.com>
2019-04-13 16:33:56 -0700

    class data hacks

--- website.py
+++ website.py
@@ -1,13 +1,13 @@
 import requests
 
-def getClassFile(url):
+def getClassFile(classnum, term):
+	url = "https://pisa.ucsc.edu/class_search/index.php"
 	response = requests.post(url,allow_redirects=False, data={
 	'action': "detail",
 	'class_data[:CLASS_NBR]': "63107",
 	'class_data[:STRM]': "2192",
 	'binds[:term]': "2192",
 	'binds[:reg_status]': "all",
-	'binds[:subject]': "CMPE",
 	'binds[:catalog_nbr_op]': "=",
 	'binds[:instr_name_op]': "=",
 	'binds[:crse_units_op]': "=",
@@ -22,7 +22,7 @@ if __name__ == '__main__':
 
 	if response.status_code == 200:
 		print("200 OK")
-		contents = parseInputs(url)
+		contents = getClassFile("63107", "2192")
 		file = open("index.html","w")
 		file.write(str(contents))
 
