a046f54200ec92772d6324f58c71255f0db4b047
Ernesto <ernestosmail45@gmail.com>
2019-04-13 12:57:17 -0700

    website

---
+++ README.txt
@@ -0,0 +1,5 @@
+			GOOGLE HOME APPLICATION
+
+	- First receive data from pisa, then translate that to something
+	- Last, create google home applciation
+
---
+++ website.py
@@ -0,0 +1,31 @@
+import os
+import requests
+
+def status():
+	if response.status_code == 200:
+		print("200 OK")
+		parseInputs()
+	
+	else:
+		print("Bad Request")
+		exit()
+
+def parseInputs():
+	response = requests.post(url,allow_redirects=False, data={
+	'action': "detail",
+	'class_data[:STRM]': "2192",
+	'class_data[:CLASS_NBR]': "63107",
+	'binds[:term]': "2192",
+	'binds[:reg_status]': "all",
+	'binds[:subject]': "CMPE"
+	})
+
+
+
+
+url = "https://pisa.ucsc.edu/class_search/index.php"
+response = requests.get(url)
+status()
+json_response = response.json()
+
+print(response.content)
